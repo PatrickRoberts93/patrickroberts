d051152298e101a5dcd4ee86c655fcf5d5c075f8
Patrick Roberts <patrickroberts93@gmail.com>
2018-04-23 10:38:51 +1000

    replaced scroll to top with svg

--- js/main.js
+++ js/main.js
@@ -58,14 +58,14 @@ $('a[href*="#"]')
   //Check to see if the window is top if not then display button
 	$(window).scroll(function(){
 		if ($(this).scrollTop() > 100) {
-			$('.scrollToTop').fadeIn();
+			$('.scroll-top').fadeIn();
 		} else {
-			$('.scrollToTop').fadeOut();
+			$('.scroll-top').fadeOut();
 		}
 	});
 
 	//Click event to scroll to top
-	$('.scrollToTop').click(function(){
+	$('.scroll-top').click(function(){
 		$('html, body').animate({scrollTop : 0},800);
 		return false;
 	});
